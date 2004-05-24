a7f67f3d1290165fd8467c50eb546744392d3ec2
pfrancq <pfrancq@f0708bf3-cd2b-0410-b9ab-d8e55ba07f82>
2004-05-24 17:47:51 +0000

    - Cleanup in the comments beginning a code file.

--- rvlsi/rinstvlsi.h
+++ rvlsi/rinstvlsi.h
@@ -11,10 +11,6 @@
 	Authors:
 		Pascal Francq (pfrancq@ulb.ac.be).
 
-	Version $Revision$
-
-	Last Modify: $Date$
-
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU Library General Public
 	License as published by the Free Software Foundation; either
