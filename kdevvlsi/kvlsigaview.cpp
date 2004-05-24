3abfcfe759523795ec97a2fe2d0ae33779d32423
pfrancq <pfrancq@e78583e5-cd2b-0410-bfe4-f9bea44749e9>
2004-05-24 17:47:51 +0000

    - Cleanup in the comments beginning a code file.

--- kdevvlsi/kvlsigaview.cpp
+++ kdevvlsi/kvlsigaview.cpp
@@ -6,10 +6,6 @@
 
   (c) 2001 by P. Francq.
 
-  Version $Revision$
-
-  Last Modify: $Date$
-
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
