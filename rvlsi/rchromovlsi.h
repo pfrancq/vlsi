c398c01c11dc69e933db340396494a914f08feef
pfrancq <pfrancq@f0708bf3-cd2b-0410-b9ab-d8e55ba07f82>
2011-01-24 16:10:27 +0000

    - Update from 2010 tp 2011.

--- rvlsi/rchromovlsi.h
+++ rvlsi/rchromovlsi.h
@@ -6,7 +6,7 @@
 
 	Chromosome for VLSI placement GA - Header
 
-	Copyright 1998-2010 by Pascal Francq (pascal@francq.info).
+	Copyright 1998-2011 by Pascal Francq (pascal@francq.info).
 	Copyright 1998-2003 by the Université Libre de Bruxelles (ULB).
 
 	This library is free software; you can redistribute it and/or
